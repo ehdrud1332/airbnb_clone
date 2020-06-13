1330567ec09fbe6841ea5703b52dbbb649b2566a
CastalJaemin <ehdrud1332@gmail.com>
2020-06-13 12:00:18 +0900

    Sign In #1

--- screens/Auth/SignIn.js
+++ screens/Auth/SignIn.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import styled from 'styled-components/native';
+import Btn from '../../components/Auth/Btn';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
+const TextInput = styled.TextInput`
+
+`;
 
 export default () => {
+
+    const handleSubmit = () => alert("Sending data");
+
     return (
-        <View style={{ justifyContent: "center", alignItems: "center", flex: 1}}>
-            <Text>Sign In</Text>
-        </View>
+        <Container>
+            <TextInput placeholder="UserName"></TextInput>
+            <TextInput placeholder="Password" secureTextEntry></TextInput>
+            <Btn text={"Sign In"} accent onPress={handleSubmit} />
+        </Container>
     );
 };
 

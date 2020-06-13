a813d9ce524a34cda9d817acf1924071cc25c137
CastalJaemin <ehdrud1332@gmail.com>
2020-06-13 11:34:03 +0900

    Welcome Screen #1

--- navigaion/Auth.js
+++ navigaion/Auth.js
@@ -5,16 +5,34 @@ import {Ionicons} from '@expo/vector-icons';
 import Welcome from '../screens/Welcome';
 import SignIn from "../screens/SignIn";
 import SignUp from "../screens/SignUp";
-
+import BackBtn from '../components/Auth/BackBtn';
 const Auth = createStackNavigator();
+
 
 const isAndroid = Platform.OS === 'android';
 
 export default () => (
 
-    <Auth.Navigator>
+    <Auth.Navigator
+        //아래서 위로 올라가는 애니메이
+        mode="model"
+        screenOptions={{
+            headerBackTitleVisible: false,
+            headerTransparent: true,
+            headerBackImage: () => <BackBtn />
 
-        <Auth.Screen name="Welcome" component={Welcome} />
+        }}
+    >
+
+        <Auth.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{
+                headerTitleStyle: {
+                    color: "white"
+                }
+            }}
+        />
         <Auth.Screen name="SignIn" component={SignIn} />
         <Auth.Screen name="SignUp" component={SignUp} />
     </Auth.Navigator>

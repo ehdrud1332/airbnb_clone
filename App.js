f8779bbe1113d386576cafdd9fe4719809a65e37
CastalJaemin <ehdrud1332@gmail.com>
2020-06-13 10:12:55 +0900

    cache fonts, Images

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Text, Image, View} from 'react-native';
+import {AppLoading} from 'expo';
+import {Asset} from 'expo-asset';
+import * as Font from 'expo-font';
+import {Ionicons} from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const cacheImages = images =>
+    images.map(image => {
+        if(typeof image === 'string') {
+            return Image.prefetch(image);
+        } else {
+            return Asset.fromModule(image).downloadAsync();
+        }
+    });
+
+const cacheFonts = fonts =>
+    fonts.map(font => [Font.loadAsync(font)]);
+
+export default function () {
+
+    const [isReady, setIsReady] = useState(false);
+    const loadAssets = async() => {
+        const images = cacheImages([
+            require("./assets/BgImage.jpeg"),
+            "http://logok.org/wp-content/uploads/2014/07/airbnb-logo-belo-219x286.png"
+        ]);
+        console.log(images);
+        const fonts = cacheFonts([Ionicons.font]);
+        return Promise.all([...images, ...fonts]);
+    }
+
+    const onFinish = () => setIsReady(true);
+
+    return isReady ? (
+        <Text>Ready</Text>
+    ) : (
+        <AppLoading
+            startAsync={loadAssets}
+            onFinish={onFinish}
+            onError={console.log}
+        />
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

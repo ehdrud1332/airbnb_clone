bccc785917f130dde8523c7cd2b1f60d39ec9a44
CastalJaemin <ehdrud1332@gmail.com>
2020-06-19 19:16:57 +0900

    add apiClient

---
+++ api.js
@@ -0,0 +1,19 @@
+import Axios from 'axios';
+
+const callApi = async (method, path, data, jwt) => {
+    const headers = {
+        Authorization: jwt,
+        "Content-Type" : "application/json"
+    };
+
+    const baseUrl = "https://127.0.0.1:2323/";
+    const fullUrl = `${baseUrl}${path}`;
+    if(method === "get" || method === "delete") {
+        return Axios[method](fullUrl, {headers});
+    } else {
+        return Axios[method](fullUrl, data, {headers});
+    }
+
+};
+
+export const createAccount = form => callApi("post", "/user/signup", form);
